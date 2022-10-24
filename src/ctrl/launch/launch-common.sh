d00d7fecbdfdd76e66d51be90b647992dd64b02c
jorufua <joru.fua@gmail.com>
2022-10-24 15:21:27 -0500

    Add files via upload

---
+++ src/ctrl/launch/launch-common.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+px4_dir=$(pwd)/src/Firmware
+source $px4_dir/Tools/setup_gazebo.bash $px4_dir $px4_dir/build/posix_sitl_default
+
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$px4_dir
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$px4_dir/Tools/sitl_gazebo
